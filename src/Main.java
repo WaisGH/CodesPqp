866ddd5f3189aa6c752b171c1e67417811b5a7ff
WaisGW <gabriel.sango11@gmail.com>
2025-11-18 14:49:24 -0300

    ATUALIZAÇÃO 8.0

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Usar o caminho fixo
-        String caminhoPrograma = "C:\\Users\\natna\\Downloads\\CodesPqp-master\\CodesPqp-master\\src\\programa.cpqp";
+        String caminhoPrograma = "C:\\Users\\Usuario\\Downloads\\CodesPqp-master\\CodesPqp-master\\src\\programa.cpqp";
 
         String caminhoHtml = "arvore.html";
 
